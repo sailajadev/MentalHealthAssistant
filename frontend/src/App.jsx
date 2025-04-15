86d690ca993110bf529cdcd9827023a3f066d987
akulasreenubabu <akula.sreenubabu@gmail.com>
2025-04-14 21:12:42 -0500

    Fix a bug related to hardcoded localhost url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   const [advice, setAdvice] = useState('');
 
   const handlePredict = async () => {
-    const res = await fetch('http://localhost:5000/api/predict', {
+    const res = await fetch('https://mentalhealthassistant.onrender.com/api/predict', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ text: predictText }),
@@ -32,7 +32,7 @@ function App() {
   };
 
   const handleGenerate = async () => {
-    const res = await fetch('http://localhost:5000/api/generate', {
+    const res = await fetch('https://mentalhealthassistant.onrender.com/api/generate', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ text: adviceText }),
